5db690d977ed4fe13e13dcdc2dafd8d8f5a12d0a
aevans <aevans@starbucks.com>
2018-04-14 14:29:44 -0700

    initial commit ✨

---
+++ src/home/image.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import '../App.css';
+
+class Button extends Component {
+  render() {
+    return (
+      <div className="btn">
+        <button type="tel">480-382-7552</button>
+      </div>
+    );
+  }
+}
+
+export default Button;
